c64619fe839f92f1908a9cbc7000b585eceb7a61
Michael Kroon <kroonology@gmail.com>
2019-09-10 00:30:01 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -2,25 +2,31 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    grocery_items: [
+      { id: 1, name: "Eggs", complete: true },
+      { id: 2, name: "Cheese", complete: false },
+      { id: 3, name: "Bread", complete: false }
+    ]
+  };
+
+  renderListItems = () => {
+    const {grocery_items} = this.state;
+    return grocery_items.map(
+      item => <li key={item.id}>{item.name}</li>
+    )
+  };
+
+  render() {
+    return (
+      <div>
+        <ul>
+          {this.renderListItems()}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
